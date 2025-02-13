1f1d1e9762af594b3763bb39d8270e4a4bb6ab4b
Nandani Yadav <nandaniy200@gmail.com>
2025-02-13 05:37:11 +0530

    khel of State

---
+++ learningreact/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import State from './Components/Practic/State';
+
+function App() {
+  return (
+    <div className="App">
+     <h3>Learn Raect</h3>
+     <State/>
+    </div>
+  );
+}
+
+export default App;
